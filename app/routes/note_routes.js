88c35839206108001528b955b9eb1d04518d4751
Sivivatu <Sivivatu@users.noreply.github.com>
2018-01-08 15:36:11 +0000

    config changes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,13 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, results) => {
       // You'll create note here
-      console.log(req.body)
-      res.send('Hello')
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
